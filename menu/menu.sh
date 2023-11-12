357819b9ecf337e7c7adb3a2694975d78b127a10
Aliputravip <143361238+putramand@users.noreply.github.com>
2023-11-12 19:38:47 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -291,7 +291,7 @@ mai="datediff "$Exp" "$DATE""
 echo -e "        ${BICyan}┌─────────────────────────────────────┐${NC}"
 echo -e "        ${BICyan}│$NC Version       : $(cat /opt/.ver) Last Update ${NC}"
 echo -e "        ${BICyan}│$NC Expiry In     : $(( (d1 - d2) / 86400 )) Days $NC"
-echo -e "        ${BICyan}│$NC User          : $(curl -sS https://raw.githubusercontent.com/putramand/permission/main/ipmini | grep $MYIP | awk '{print $2}')"
+echo -e "        ${BICyan}│$NC User          : $(cat /etc/.$Name.ini)
 echo -e "        ${BICyan}└─────────────────────────────────────┘${NC}"
 echo
 read -p " Select menu : " opt
