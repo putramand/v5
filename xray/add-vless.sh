87c0d9f157cfea37a23a403745b36022bad121b3
Aliputravip <143361238+putramand@users.noreply.github.com>
2023-11-12 20:27:59 +0700

    Update add-vless.sh

--- xray/add-vless.sh
+++ xray/add-vless.sh
@@ -39,7 +39,7 @@ clear
 uuid=$(cat /proc/sys/kernel/random/uuid)
 read -p "Limit ip (DEVICE) : " Limit
 read -p "Limit bw (GB) : " bw
-read -p "Limit quota (GB) : quota
+read -p "Limit quota (GB) : " quota
 read -p "Expired (days): " masaaktif
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#vless$/a\#& '"$user $exp"'\
@@ -70,10 +70,10 @@ echo -e "Link none TLS  : ${vlesslink2}" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 echo -e "Link GRPC      : ${vlesslink3}" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "Limit ip    : $Limit (DEVIC)"
-echo -e "Limit bw : $bw (GB)"
-echo -e "Limit quota : $quota (GB)"
-echo -e "Expired On     : $exp (HARI)" | tee -a /etc/log-create-user.log
+echo -e "Limit ip      : $Limit (DEVIC)"
+echo -e "Limit bw      : $bw (GB)"
+echo -e "Limit quota   : $quota (GB)"
+echo -e "Expired On    : $exp (HARI)" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 echo "" | tee -a /etc/log-create-user.log
 read -n 1 -s -r -p "Press any key to back on menu"
